e335036c98ea5bfde8c65eabdb3c71d7c595dbfa
Alperen Keleş <alpkeles99@gmail.com>
2020-12-11 19:01:43 +0300

    Add files via upload

---
+++ ders_0.md
@@ -0,0 +1,37 @@
+# primitive types
+(copied)
+- int
+- char
+- double
+- str(len < 256)
+- bool
+
+# In C:
+    integer -> 32
+    000111...1110
+    2^32
+    -2^31 / 2^31 - 1
+# In Python
+    integer -> unbounded
+    double -> unbounded
+
+# container types
+(referenced)
+
+- list                  mutable
+- tuple                 immutable
+- set                   immutable
+- dictionary            mutable
+- string(len > 256)     unmutable
+
+# Remarks:
+- set
+    -each element should be hashable
+
+# composite types
+- class
+
+
+
+
+
---
+++ lists.py
@@ -0,0 +1,59 @@
+l0 = list()
+l1 = []
+l2 = [3, 5, 7, 9]
+
+# Indexing
+l2[1]
+print(l2[1])
+
+# Length
+len(l2)
+print(len(l2))
+print(l2)
+
+# Appending element to end of the list
+l2.append(11)
+print(l2)
+# Inserting an element to middle of the list
+l2.insert(0, -1)
+print(l2)
+# List Concatenation
+
+l3 = [13]
+l2 = l2 + l3
+print(l2)
+# Pop Last Element from List
+l2.pop()
+print(l2)
+
+# Pop An Index From List
+l2.pop(2)
+print(l2)
+
+# Remove a value from list
+l2.remove(7)
+print(l2)
+
+# Backward Indexing
+print("l2[-1]", l2[-1])
+
+# Sorting List
+l2.insert(0, 10)
+print("Before Sort: ", l2)
+l2.sort()
+
+
+print("After Sort: ", l2)
+l2.sort(reverse=True)
+print("After Reverse Sort: ", l2)
+print("Last List: ", l2)
+print(sorted(l2))
+print(l2)
+
+
+
+
+# [3,  5,  7,  9]
+# |0  |1  |2  |3 |
+# |-4 |-3 |-2 |-1|
+
